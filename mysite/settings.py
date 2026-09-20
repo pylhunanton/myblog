2310ecec6606be45d89a4bfea6501ed9fdd41616
Anton Pylgun <pylhunanton@gmail.com>
2026-09-20 16:50:25 +0300

    Лабораторна робота 10

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app_blog', 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
