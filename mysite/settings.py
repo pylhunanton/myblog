42aee9ea752b3012f19619eb7eeb22fff9027a54
Anton Pylgun <pylhunanton@gmail.com>
2026-09-20 14:27:53 +0300

    Лабораторна робота 7

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$ak2r%@eqna5h=m^a95db5p5kr$+%+1ldw^nxe7s1^*1fju6td
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_blog',
 ]
 
 MIDDLEWARE = [
